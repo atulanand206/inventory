44e211a4f3f5bb2e551f57b1bd73e9a6371be9c8
Atul Anand <atulanand206@gmail.com>
2022-09-01 02:13:00 +0530

    data model

--- types/types.go
+++ types/types.go
@@ -1,5 +1,69 @@
 package types
 
+type User struct {
+	Id    string `json:"id" bson:"id"`
+	Name  string `json:"name" bson:"name"`
+	Phone string `json:"phone" bson:"phone"`
+}
+
+type Building struct {
+	Id   string `json:"id" bson:"id"`
+	Name string `json:"name" bson:"name"`
+}
+
+type SharingStatus int
+
+const (
+	Single SharingStatus = iota
+	Double
+)
+
+func (s SharingStatus) String() string {
+	switch s {
+	case Single:
+		return "Single"
+	case Double:
+		return "Double"
+	}
+	return "unknown"
+}
+
+type RoomSharing struct {
+	BuildingId    string        `json:"buildingId" bson:"buildingId"`
+	RoomNo        int           `json:"roomNo" bson:"roomNo"`
+	SharingStatus SharingStatus `json:"sharingStatus" bson:"sharingStatus"`
+}
+
+type OccStatus int
+
+const (
+	Occupied OccStatus = iota
+	Vacant
+)
+
+func (s OccStatus) String() string {
+	switch s {
+	case Occupied:
+		return "Occupied"
+	case Vacant:
+		return "Vacant"
+	}
+	return "unknown"
+}
+
+type BuildingBed struct {
+	BuildingId      string    `json:"buildingId" bson:"buildingId"`
+	BedId           string    `json:"bedId" bson:"bedId"`
+	Floor           int       `json:"floor" bson:"floor"`
+	RoomNo          int       `json:"roomNo" bson:"roomNo"`
+	OccupancyStatus OccStatus `json:"occupancyStatus" bson:"occupancyStatus"`
+}
+
+type BedUser struct {
+	BedId  string `json:"bedId" bson:"bedId"`
+	UserId string `json:"userId" bson:"userId"`
+}
+
 type Status int
 
 const (
@@ -26,6 +90,8 @@ type Machine struct {
 	Status Status `json:"status" bson:"status"`
 }
 
-type Inventory struct {
-	Machines []Machine
+type Usage struct {
+	MachineId string `json:"machineId" bson:"machineId"`
+	BedId     string `json:"bedId" bson:"bedId"`
+	Status    Status `json:"status" bson:"status"`
 }
